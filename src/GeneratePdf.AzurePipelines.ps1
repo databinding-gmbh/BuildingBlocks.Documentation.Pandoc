6d4698aa406a349e52e25c3c15d38a1f460eeb3f
thierryiseli <thierry.iseli@databinding.ch>
2020-06-08 14:09:22 +0200

    Ignore home.md if exists

--- src/GeneratePdf.AzurePipelines.ps1
+++ src/GeneratePdf.AzurePipelines.ps1
@@ -1,5 +1,10 @@
 param (
 	[string]$o = "wiki.pdf"
 )
+$homeMarkdownForGithubWiki = "Home.md"
+if (Test-Path $homeMarkdownForGithubWiki) 
+{
+  Remove-Item $homeMarkdownForGithubWiki
+}
 ren template.tex template.latex
 pandoc (get-item -path "*.md").FullName -o $o --toc -N -V margin-top=50mm -V margin-left=20mm -V margin-right=20mm -V margin-bottom=30mm --template template -V titlepage=true -V toc-own-page=true -V page-background="layout-background.pdf" -V titlepage-background="layout-title-background.pdf" -V page-background-opacity=1 -V version=1.0.0 -V lang=de -F mermaid-filter.cmd
